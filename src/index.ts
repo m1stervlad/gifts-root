b5dd4bc6360cee6c9d4628c5d226cdbf6772a32d
m1stervlad <vladislav.kirilenok@gmail.com>
2025-07-08 01:21:39 +0300

    Revert "test"
    This reverts commit ce942b4b6e6dc07d30fab7fcfa17f89ea48812f0.

--- src/index.ts
+++ src/index.ts
@@ -67,11 +67,11 @@ async function monitor() {
     const gifts = starGifts.gifts as StarGift[];
 
     const limitedGifts = gifts.filter((gift) => {
-      return !gift.limited;
+      return gift.limited;
     });
 
     const sortedLimitedGifts = limitedGifts.sort(
-      (a, b) => a.stars.toJSNumber() - b.stars.toJSNumber(),
+      (a, b) => b.stars.toJSNumber() - a.stars.toJSNumber(),
     );
 
     const notSoldOut = sortedLimitedGifts.filter(
@@ -79,9 +79,9 @@ async function monitor() {
     );
 
     status = {
-      new_gifts: notSoldOut.map((item, index) => ({
+      new_gifts: notSoldOut.map((item) => ({
         id: item.id.toString(),
-        supply: index === 0 ? 5000 : 500000,
+        supply: item.availabilityTotal!,
         price: item.stars.toJSNumber(),
       })),
       status: "ok",
